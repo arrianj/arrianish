628c547405307df193affff777d8fcae8bffb28b
Arrian Jahangiri <arrianj@gmail.com>
2020-12-06 17:24:08 -0600

    more typos oops

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
 #################
 #    IMPORTS
 #################
+
 from string_with_arrows import *
 
 #################
@@ -205,7 +206,6 @@ class UnaryOpNode:
         self.node = node
         self.pos_start = self.op_tok.pos_start
         self.pos_end = node.pos_end
-
 
     def __repr__(self):
         return f'({self.op_tok}, {self.node})'
