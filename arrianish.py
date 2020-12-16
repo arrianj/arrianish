80bdc492f98fd039c82782d3c60f2b5686e996ef
Arrian Jahangiri <arrianj@gmail.com>
2020-12-16 11:23:42 -0600

    added initial string functionality

--- arrianish.py
+++ arrianish.py
@@ -97,6 +97,7 @@ class Position:
 
 tt_int          = 'int'
 tt_float        = 'float'
+tt_string       = 'string'
 tt_identifier   = 'identifier'
 tt_keyword      = 'keyword'
 tt_plus         = 'plus'
@@ -179,6 +180,8 @@ class Lexer:
                 tokens.append(self.make_number())
             elif self.current_char in letters:
                 tokens.append(self.make_identifier())
+            elif self.current_char == '"':
+                tokens.append(self.make_string())
             elif self.current_char == '+':
                 tokens.append(Token(tt_plus, pos_start = self.pos))
                 self.advance()
@@ -237,6 +240,31 @@ class Lexer:
             return Token(tt_int, int(num_str), pos_start, self.pos)
         else:
             return Token(tt_float, float(num_str), pos_start, self.pos)
+
+    def make_string(self):
+        string = ''
+        pos_start = self.pos.copy()
+        escape_character = False
+        self.advance()
+
+        escape_characters = {
+            'n': '\n',
+            't': '\t'
+        }
+
+        while self.current_char != None and (self.current_char != '"' or escape_character):
+            if escape_character:
+                string += escape_characters.get(self.current_char, self.current_char)
+            else:
+                if self.current_char == '\\':
+                    escape_character = True
+                else:
+                    string += self.current_char
+            self.advance()
+            escape_character = False
+
+        self.advance()
+        return Token(tt_string, string, pos_start, self.pos)
 
     def make_identifier(self):
         id_str = ''
@@ -309,6 +337,12 @@ class Lexer:
 #######################################
 
 class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
+
+class StringNode:
     def __init__(self, tok):
         self.tok = tok
         self.pos_start = self.tok.pos_start
@@ -663,6 +697,11 @@ class Parser:
             res.register_advancement()
             self.advance()
             return res.success(NumberNode(tok))
+
+        if tok.type in (tt_string):
+            res.register_advancement()
+            self.advance()
+            return res.success(StringNode(tok))
 
         elif tok.type == tt_identifier:
             res.register_advancement()
@@ -1099,6 +1138,35 @@ class Number(Value):
     def __repr__(self):
         return str(self.value)
 
+class String(Value):
+    def __init__(self, value):
+        super().__init__
+        self.value = value
+
+    def added_to(self, other):
+        if isinstance(other, String):
+            return String(self.value + other.value).set_context(self.context), None
+        else:
+            return None, Value.illegal_operation(self, other)
+
+    def multed_by(self, other):
+        if isinstance(other, Number):
+            return String(self.value * other.value).set_context(self.context), None
+        else:
+            return None, Value.illegal_operation(self, other)
+    
+    def is_true(self):
+        return len(self.value) > 0
+
+    def copy(self):
+        copy = String(self.value)
+        copy.set_pos(self.pos_start, self.pos_end)
+        copy.set_context(self.context)
+        return copy
+    
+    def __repr__(self):
+        return f'"{self.value}"'
+
 class Function(Value):
     def __init__(self, name, body_node, arg_names):
         super().__init__()
@@ -1195,6 +1263,11 @@ class Interpreter:
     def visit_NumberNode(self, node, context):
         return RTResult().success(
             Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+        )
+
+    def visit_StringNode(self, node, context):
+        return RTResult().success(
+            String(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
         )
 
     def visit_VarAccessNode(self, node, context):
