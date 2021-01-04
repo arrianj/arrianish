054e0e5606f6bad3da32a3bf3ba68b09c6f6cb45
Arrian Jahangiri <arrianj@gmail.com>
2021-01-03 18:01:40 -0600

    updated shell to support statements, multi-line input, and fixed invalid syntax error from mistakenly arising

--- shell.py
+++ shell.py
@@ -2,7 +2,14 @@ import arrianish
 
 while True:
     text = input('arrianish > ')
+    # parses trimmed input to prevent an incorrect invalid syntax exception in the event of empty inputs
+    if text.strip() == "": continue
     result, error = arrianish.run('<stdin>', text)
 
     if error: print(error.as_string())
-    elif result: print(repr(result))
+    elif result:
+        # prevents result being shown in a list if it is the only element in the result
+        if len(result.elements) == 1:
+            print (repr(result.elements[0]))
+        else:
+            print(repr(result))
